0f9df10a7f7ae4c38cf5d305a13d2e5440ea95bc
apbaruffaldi <100204802+apbaruffaldi@users.noreply.github.com>
2025-01-26 22:37:13 -0300

    test ci/cd

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  i18n: {
+  i18n: process.env.NEXT_EXPORT ? {} : {
     locales: ['en', 'es'], // Idiomas que tu app soporta
     defaultLocale: 'en',    // Idioma por defecto
     localeDetection: false, // Para evitar que Next.js detecte el idioma automáticamente
