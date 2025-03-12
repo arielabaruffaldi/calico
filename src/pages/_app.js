156cc30c48d37639b90d29ea1a3453f583b5b069
arielabaruffaldi <baruffaldiariela@gmail.com>
2025-03-12 20:02:05 -0300

    fix translations

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -18,9 +18,9 @@ function AppContent({ Component, pageProps }) {
         {/* seo begin */}
         <title>{AppData.settings.siteName} - {intl.formatMessage({ id: "page.seo.title" })}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content={intl.formatMessage({ id: "pages.seo.description" })} />
+        <meta name="description" content={intl.formatMessage({ id: "page.seo.description" })} />
       
-        <meta name="keywords" content={intl.formatMessage({ id: "pages.seo.keywords" })} />
+        <meta name="keywords" content={intl.formatMessage({ id: "page.seo.keywords" })} />
         <meta name="author" content={AppData.settings.siteName} />
         <meta name="robots" content="index, follow" />
         <meta name="googlebot" content="index, follow" />
@@ -31,7 +31,7 @@ function AppContent({ Component, pageProps }) {
         <meta name="baidu" content="index, follow" />
         <meta name="sitemap" content="/sitemap.xml" />
         <meta name="google-site-verification" content="google-site-verification=google-site-verification" />     
-        <meta property="og:description" content={intl.formatMessage({ id: "pages.seo.description" })} />
+        <meta property="og:description" content={intl.formatMessage({ id: "page.seo.description" })} />
         <meta property="og:type" content="website" />
         <meta property="og:title" content={AppData.settings.siteName} />
         <meta property="og:url" content="https://www.calico.com" />
